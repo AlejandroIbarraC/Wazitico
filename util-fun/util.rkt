aa2ce78b3d6fd187da68492addbdd2fd38c74fb0
Esteban Alvarado <estalvgs1999@gmail.com>
2019-08-15 01:34:27 -0600

    Util algs minor changes

--- util-fun/util.rkt
+++ util-fun/util.rkt
@@ -12,7 +12,6 @@
 #lang racket
 
 (require "../logic-scr/graph.rkt")
-
 (provide apply-fun
          sort-by-weight)
 
@@ -27,38 +26,29 @@
 
 ;; (sort-by-weight list graph)
 ;; Order all routes from least to greatest according to your weight.
-;; Implemented as a quick sort.
-
-;; Quicksort
-;; @param list_ : list to order
+;; Implemented as a quick sort
 (define (sort-by-weight list_ graph)
   (cond ((null? list_)
           '())
         (else
-          (append (sort-by-weight (less-than (car list_) (cdr list_)) graph)
+          (append (sort-by-weight (less-than (car list_) (cdr list_) graph) graph)
                   (list (car list_))
-                  (sort-by-weight (greater-than (car list_) (cdr list_)) graph)))))
+                  (sort-by-weight (greater-than (car list_) (cdr list_) graph) graph)))))
 
-;; less-than
-;; @param pivot : item with which it compares
-;; @param list_ : remaining list
 (define (less-than pivot list_ graph)
   (cond ((null? list_)
           '())
         ((> (path-weight pivot graph)
             (path-weight (car list_) graph))
-          (cons (car list_) (less-than pivot (cdr list_))))
+          (cons (car list_) (less-than pivot (cdr list_) graph)))
         (else
-          (less-than pivot (cdr list_)))))
+          (less-than pivot (cdr list_) graph))))
 
-;; greater-than
-;; @param pivot : item with which it compares
-;; @param list_ : remaining list
 (define (greater-than pivot list_ graph)
   (cond ((null? list_)
           '())
         ((<= (path-weight pivot graph)
              (path-weight (car list_) graph))
-          (cons (car list_) (greater-than pivot (cdr list_))))
+          (cons (car list_) (greater-than pivot (cdr list_) graph)))
         (else
-          (greater-than pivot (cdr list_)))))
+          (greater-than pivot (cdr list_) graph))))
